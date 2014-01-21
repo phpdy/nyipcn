0a3b068130e35dbe47b7906956002b7649076c5b
diyong <dyong525@163.com>
2014-01-21 15:46:23 +0800

    uup

---
+++ www/common/title.php
@@ -0,0 +1,21 @@
+<div id="header">
+
+<div id="navbox">
+	<span class="topbar">
+	<a href="http://www.sogou.com" target="_blank">首页</a> | 
+	<a href="http://fuwu.sogou.com" target="_blank">服务帮助</a> | 
+	<a href="login.jsp">登录</a>
+	</span>
+	
+	<a href="#" class="logo"><img src="images/logo.jpg"></a>
+
+	<ul class="mainnav">
+		<li><a href="login.jsp">首 页</a></li>
+		<li><a href="product.jsp">产品介绍</a></li>
+		<li>常见问题</li>
+		<li>开发文档</li>
+		<li><a href="apply.jsp">API申请</a></li>
+	</ul>
+<!--	<span class="account"><img src="images/tel.jpg"></span>-->
+</div>
+</div>
